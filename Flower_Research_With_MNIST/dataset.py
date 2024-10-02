2ee05f867f785c1bb856f3bd05436125796c9773
Vinícius Filgueiras <80530912+Thevizin@users.noreply.github.com>
2024-10-02 01:28:00 -0300

    MiniUpdate2.py

--- Flower_Research_With_MNIST/dataset.py
+++ Flower_Research_With_MNIST/dataset.py
@@ -18,30 +18,24 @@ def get_mnist(data_path: str = "./data"):
 def prepare_dataset(num_partitions: int, batch_size: int, val_ratio: float = 0.1):
     """Download MNIST and generate IID partitions."""
 
-    # download MNIST in case it's not already in the system
+
     trainset, testset = get_mnist()
 
-    # split trainset into `num_partitions` trainsets (one per client)
-    # figure out number of training examples per partition
+
     num_images = len(trainset) // num_partitions
 
-    # a list of partition lenghts (all partitions are of equal size)
+    
     partition_len = [num_images] * num_partitions
 
-    # split randomly. This returns a list of trainsets, each with `num_images` training examples
-    # Note this is the simplest way of splitting this dataset. A more realistic (but more challenging) partitioning
-    # would induce heterogeneity in the partitions in the form of for example: each client getting a different
-    # amount of training examples, each client having a different distribution over the labels (maybe even some
-    # clients not having a single training example for certain classes). If you are curious, you can check online
-    # for Dirichlet (LDA) or pathological dataset partitioning in FL. A place to start is: https://arxiv.org/abs/1909.06335
+   
     trainsets = random_split(
         trainset, partition_len, torch.Generator().manual_seed(2023)
     )
 
-    # create dataloaders with train+val support
+    
     trainloaders = []
     valloaders = []
-    # for each train set, let's put aside some training examples for validation
+    
     for trainset_ in trainsets:
         num_total = len(trainset_)
         num_val = int(val_ratio * num_total)
@@ -51,8 +45,7 @@ def prepare_dataset(num_partitions: int, batch_size: int, val_ratio: float = 0.1
             trainset_, [num_train, num_val], torch.Generator().manual_seed(2023)
         )
 
-        # construct data loaders and append to their respective list.
-        # In this way, the i-th client will get the i-th element in the trainloaders list and the i-th element in the valloaders list
+        
         trainloaders.append(
             DataLoader(for_train, batch_size=batch_size, shuffle=True, num_workers=2)
         )
@@ -60,14 +53,7 @@ def prepare_dataset(num_partitions: int, batch_size: int, val_ratio: float = 0.1
             DataLoader(for_val, batch_size=batch_size, shuffle=False, num_workers=2)
         )
 
-    # We leave the test set intact (i.e. we don't partition it)
-    # This test set will be left on the server side and we'll be used to evaluate the
-    # performance of the global model after each round.
-    # Please note that a more realistic setting would instead use a validation set on the server for
-    # this purpose and only use the testset after the final round.
-    # Also, in some settings (specially outside simulation) it might not be feasible to construct a validation
-    # set on the server side, therefore evaluating the global model can only be done by the clients. (see the comment
-    # in main.py above the strategy definition for more details on this)
+    
     testloader = DataLoader(testset, batch_size=128)
 
     return trainloaders, valloaders, testloader
